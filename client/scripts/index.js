429ae3b1b6f602c47fb30056b643e1fa8cdda2bc
antaww <anta.pizzetta@gmail.com>
2022-01-28 12:44:04 +0100

    keyboard listener log (need letter_used())

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,9 +1,25 @@
 let elements = document.querySelectorAll(".icon");
-elements.forEach(element => {
-    element.addEventListener("click", () => {
+
+function letter_used(element) {
+    if (element.classList.contains('clicked')) {
+        console.log('already clicked ' + (element.querySelector("span").innerText))
+    } else {
         element.classList.add('clicked')
         element.classList.remove('letter')
         element.querySelector('.tooltip').style.display = "none";
         console.log(element.querySelector("span").innerText)
+    }
+}
+
+elements.forEach(element => {
+    element.addEventListener("click", () => {
+        letter_used(element);
     })
 })
+
+document.addEventListener('keydown', function (event) {
+    if ((event.key >= 'a' && event.key <= 'z') || (event.key >= 'A' && event.key <= 'Z')) {
+        console.log(event.key)
+        // letter_used(event.key) //need to translate it (element = key)
+    }
+});
