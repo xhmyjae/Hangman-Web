7643a4be21f25eb0dd8eed3b0f0f0b37a3b25f02
antaww <anta.pizzetta@gmail.com>
2022-01-29 21:37:21 +0100

    letter active done + keyboard listener

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -17,7 +17,7 @@ elements.forEach(element => {
     })
 })
 
-document.addEventListener('keydown', function (event) {
+document.addEventListener('keyup', function (event) {
     let key = event.key.toLowerCase();
     if (key >= 'a' && key <= 'z') {
         console.log(key)
@@ -28,6 +28,25 @@ document.addEventListener('keydown', function (event) {
         if (!element1) {
             return
         }
-        letter_used(element1) //need to translate it (element = key)
+        letter_used(element1)
+        element1.classList.remove('border_letter')
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    let letter = document.querySelectorAll(".letter");
+    let key = event.key.toLowerCase();
+    if (key >= 'a' && key <= 'z') {
+        let element1 = [...document.querySelectorAll("span")].find(element => {
+            return element.innerText === key.toUpperCase();
+        }).parentNode;
+        if (!element1) {
+            return
+        }
+        if (document.hasFocus()) {
+            if (!element1.classList.contains('clicked')) {
+                element1.classList.add('border_letter')
+            }
+        }
     }
 });
