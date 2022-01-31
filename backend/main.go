930c1025ee2670100e01cb37d5712ca628f751cf
Ayfri <pierre.ayfri@gmail.com>
2022-01-31 17:23:43 +0100

    refactor: Use go templates, use multiple routes.

--- backend/main.go
+++ backend/main.go
@@ -3,28 +3,50 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
-var clay Hangman
+type State struct {
+	Menu string
+	Clay Hangman
+}
+
+var state = State{Menu: "main", Clay: Hangman{}}
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	tmpl := template.Must(template.ParseFiles("../client/pages/game.html"))
+	tmpl, err := template.ParseGlob("./templates/*.gohtml")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fs := http.FileServer(http.Dir("../client/style"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fmt.Println(tmpl.DefinedTemplates())
 
+	css := http.FileServer(http.Dir("./client/style"))
+	js := http.FileServer(http.Dir("./client/scripts"))
+	resources := http.FileServer(http.Dir("./backend/resources/"))
+	http.Handle("/static/", http.StripPrefix("/static/", css))
+	http.Handle("/js/", http.StripPrefix("/js/", js))
+	http.Handle("/resources/", http.StripPrefix("/resources/", resources))
+
+	state.Clay.Init(GetRandomWord("./backend/resources/words.txt"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		state.Menu = "main"
+		tmpl.ExecuteTemplate(w, "main", state)
+	})
 
+	http.HandleFunc("/hangman", func(w http.ResponseWriter, r *http.Request) {
+		state.Menu = "game"
+		tmpl.ExecuteTemplate(w, "main", state)
+	})
 
-		println("test")
-		clay.Init(GetRandomWord("resources/words.txt"))
-
-		tmpl.Execute(w, clay)
+	http.HandleFunc("/rules", func(w http.ResponseWriter, r *http.Request) {
+		state.Menu = "rules"
+		tmpl.ExecuteTemplate(w, "main", state)
 	})
 
 	http.ListenAndServe(":8999", nil)
